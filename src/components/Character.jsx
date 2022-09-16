ff028bc611dfceb870652c2f72dac7b13ee9aa9a
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2022-09-16 00:10:32 -0500

    Feature (logic components): Create the logic for each component with its respective props and states.

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -2,55 +2,61 @@ import React from "react";
 // import { StyledSection } from "../../styles/components/CharacterItem";
 
 const Character = ({character}) => {
+            return(
+                <main>
+                    {character.map(character =>(
 
-    return(
-            <main>
+                    <div key={character.id}>
 
-                <img alt="Imagen of characters af the Rick and Morty" src={character.image}/>
-
-                <div>
-
-                    <header>
-                        <h2>{character.name}</h2>
-                        <span>{`ID: ${character.id}`}</span>
-                    </header>
-                    <section>
-
-                        <div>
-                            <span>Status:</span>
-                            {character.status}
-                        </div>
-                        <div>
-                            <span>Species:</span>
-                            {character.species}
-                        </div>
-                        <div>
-                            <span>Type:</span>
-                            {character.type}
-                        </div>
-                        <div>
-                            <span>Gender:</span>
-                            {character.gender}
-                        </div>
-                        <div>
-                            <span>Origin:</span>
-                            {character.origin.dimension}
-                        </div>
-                        <div>
-                            <span>Location:</span>
-                            {character.location.name}
-                        </div>
-                        <div>
-                            <span>Created at:</span>
-                            {character.created}
-                        </div>
-                    </section>
-                </div>
-            </main>
-
-
+                        <img alt="Imagen of characters af the Rick and Morty" src={character.image}/>
         
-    )
+                        <div>
+        
+                            <header>
+                                <h2>{character.name}</h2>
+                                <span>{`ID: ${character.id}`}</span>
+                            </header>
+                            <section>
+        
+                                <div>
+                                    <span>Status:</span>
+                                    {character.status}
+                                </div>
+                                <div>
+                                    <span>Species:</span>
+                                    {character.species}
+                                </div>
+                                <div>
+                                    <span>Type:</span>
+                                    {character.type}
+                                </div>
+                                <div>
+                                    <span>Gender:</span>
+                                    {character.gender}
+                                </div>
+                                {/* <div>
+                                    <span>Origin:</span>
+                                    {character.origin.dimension}
+                                </div> */}
+                                <div>
+                                    <span>Location:</span>
+                                    {character.location.name}
+                                </div>
+                                <div>
+                                    <span>Created at:</span>
+                                    {character.created}
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                    ))
+
+                    }
+                </main>
+                ) 
+            
+        
+    
 }
 
 export { Character}
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Logo = ({className}) =>{
-    const urlLogo= 'http://www.realhost.com.mx/site/img/LogoRealhostBlue.png';
+    const urlLogo= 'http://www.realhost.com.mx/site/img/LogoOriginal2.png';
     return(
         <a href='http://www.realhost.com.mx/site/' className={className}>
             <img src={urlLogo} alt='Logo de REALHOST'  className=''/>
